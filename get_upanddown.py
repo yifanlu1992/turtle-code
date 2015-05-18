5184999d14f4d9bff3d53796aafae9a14e7a83c5
dzbhhz <dzbhhz@gmail.com>
2015-05-18 10:49:44 -0400

    new

---
+++ get_upanddown.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  4 12:46:52 2015
+
+@author: zhaobin
+"""
+'find out which are downcasts and upcasts'
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from datetime import datetime,timedelta
+def find_zero(a,List):
+    start=len(List)-3
+    if List[a]<2:
+        for i in range(a,len(List)-2):
+            if List[i]<List[i+1] and List[i]<2:
+                if List[i+1]<List[i+2] and List[i+1]>=2 :
+                    start=i
+                    break                            #find farest on surface before diving
+        for i in range(start,len(List)-2):
+            if List[i]>=2 and List[i]>List[i+1]:
+               if List[i+1]<2 and List[i+1]>List[i+2]:
+                   break                             #find nearest on surface after diving
+    return [start,i+1]
+#####################################
+data=pd.read_csv('turtle_12487_tdr.csv')
+depth=pd.Series(data['Depth'])
+time=pd.Series(data['Date'])                         
+for i in range(len(data.index)-1,0,-1):
+    if depth[i]==0:
+        if depth[i-1]>0:
+            T=i
+            break                        #depth of last loaction is not zero.
+print T
+indx=[]
+for i in range(T):
+    if 1+i<T:              #get rid of the last one
+        if depth[i]<2:      # use 2 beacuse some dives don`t go to air
+            I=find_zero(i,depth)
+            indx.append(I)
+    if i%1000==0:    
+        print 'I',i 
+Indx = [list(x) for x in set(tuple(x) for x in indx)]  #get rid of repeated data
+Indx.sort()
+INDX=[]
+for i in range(len(Indx)):
+    Max=max(depth[Indx[i][0]:Indx[i][1]+1])
+    Max_indx=[]
+    for j in range(Indx[i][0],Indx[i][1]+1):
+        if int(depth[j])==int(Max):
+             Max_indx.append(j)    
+    INDX.append(Max_indx)
+    print 'i',i
+
+downcast,upcast=[],[]
+for i in range(len(Indx)):
+    downcast.append([Indx[i][0],INDX[i][0]])
+    upcast.append([INDX[i][-1],Indx[i][-1]])
+data=pd.DataFrame(range(len(upcast)))
+data['down']=pd.Series(downcast)
+data['up']=pd.Series(upcast)
+data.to_csv('12487up_down.csv')
---
+++ upanddown_speed.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 13 09:19:10 2015
+
+@author: zhaobin
+"""
+'1)plot speed of upcasts and downcasts'
+'2)plot change of temperature vs speed'
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from datetime import datetime
+from turtleModule import str2ndlist
+############################################################
+second_data=pd.read_csv('turtle_12487_tdr.csv')
+depth=pd.Series(second_data['Depth'])
+temp=pd.Series(second_data['temp'])
+time=pd.Series(second_data['Date'])
+
+for i in second_data.index:
+    time[i]=datetime.strptime(time[i],'%Y/%m/%d %H:%M:%S')
+
+data=pd.read_csv('12487up_down.csv')
+up=pd.Series(str2ndlist(data['up'],bracket=True))
+down=pd.Series(str2ndlist(data['down'],bracket=True))
+indx=[]
+for i in data.index:
+    if depth[down[i][-1]]>20:       #depth>20m
+        if depth[up[i][0]]>20:
+            indx.append(i)
+Down_mean_temp,Up_mean_temp,Down_min_temp,Up_min_temp,Down_max_temp,Up_max_temp=[],[],[],[],[],[]
+diff_temp_up,diff_temp_down=[],[]
+Down_speed,Up_speed=[],[]
+for i in indx[1:len(indx)]:
+    a=temp[down[i][0]:down[i][-1]]
+    b=temp[up[i][0]:up[i][-1]]
+    Down_mean_temp.append(np.mean(a))
+    Up_mean_temp.append(np.mean(b))
+    Down_min_temp.append(min(a))
+    Up_min_temp.append(min(b))
+    Down_max_temp.append(max(a))
+    Up_max_temp.append(max(b))
+    aa=temp[down[i][0]]-temp[down[i][-1]]
+    bb=temp[up[i][-1]]-temp[up[i][0]]    
+    diff_temp_down.append(aa)
+    diff_temp_up.append(bb)
+    
+    down_speed,up_speed=[],[]
+    a=(depth[down[i][-1]]-depth[down[i][0]])/(time[down[i][-1]]-time[down[i][0]]).total_seconds()
+    b=(depth[up[i][0]]-depth[up[i][-1]])/(time[up[i][-1]]-time[up[i][0]]).total_seconds()
+    down_speed.append(a)
+    up_speed.append(b)
+    Down_speed.append(down_speed)
+    Up_speed.append(up_speed)
+for i in range(len(Down_speed)):
+    Down_speed[i]=round(Down_speed[i][0],3)  
+    Up_speed[i]=round(Up_speed[i][0],3)
+slow_up_speed,slow_down_speed=[],[]
+for i in range(len(Up_speed)):        #calculate slow speed
+    if Up_speed[i]<=0.1:
+        slow_up_speed.append(Up_speed[i])
+    if Down_speed[i]<=0.1:
+        slow_down_speed.append(Down_speed[i])
+fig=plt.figure()
+ax=fig.add_subplot(121)
+ax.hist(Down_speed)
+ax.set_ylabel('number of dives',fontsize=20)
+ax.set_xlabel('average speed(m/s)',fontsize=20)
+ax.set_title('downcasts',fontsize=25)
+ax1=fig.add_subplot(122)
+ax1.hist(Up_speed)
+ax1.set_xlabel('average speed(m/s)',fontsize=20)
+ax1.set_ylabel('number of dives',fontsize=20)
+ax1.set_title('upcasts',fontsize=25)
+fig1=plt.figure()
+ax=fig1.add_subplot(121)
+ax.scatter(diff_temp_down,Down_speed)
+ax.set_xlabel('change in temperature',fontsize=20)
+ax.set_ylabel('average speed(m/s)',fontsize=20)
+ax.set_title('downcasts',fontsize=25)
+ax1=fig1.add_subplot(122)
+ax1.scatter(diff_temp_up,Up_speed)
+ax1.set_xlabel('change in temperature',fontsize=20)
+ax1.set_title('upcasts',fontsize=25)
+plt.show()
+print 'mean(mean temperature in downcasts)',round(np.mean(Down_mean_temp),3)
+print 'mean(min temperature in downcasts)',round(np.mean(Down_min_temp),3)
+print 'mean(max temperature in downcasts)',round(np.mean(Down_max_temp),3)
+print 'mean(mean temperature in upcasts)',round(np.mean(Up_mean_temp),3)
+print 'mean(min temperature in upcasts)',round(np.mean(Up_min_temp),3)
+print 'mean(max temperature in upcasts)',round(np.mean(Up_max_temp),3)
+print "'slow' speed in upcasts is",round(float(len(slow_up_speed))/len(Up_speed)*100,2),'%'
+print "'slow' speed in downcasts is",round(float(len(slow_down_speed))/len(Down_speed)*100,2),'%'
